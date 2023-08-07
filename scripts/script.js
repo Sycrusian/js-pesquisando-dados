38573f04abcc831c9f9575cb20d509b76f16dd75
Sycrusian <sycrus@protonmail.com>
2023-08-07 17:38:29 -0300

    Atividade - Pesquisando por dados

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 function procuraSobremesa(valorDoInput) {
     /* Desenvolva sua lógica a partir aqui */
-    
-    return /* É necessário retornar uma lista de produtos */
+    const searchResult = listaDeSobremesas.filter(sobremesa => sobremesa.nome.toLowerCase().includes(valorDoInput.toLowerCase()));
+    return searchResult; /* É necessário retornar uma lista de produtos */
 }
